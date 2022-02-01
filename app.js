b85227f56105d1b85e0cc0b3917ff08ab414bbe5
Conor Hagan <conorhagan7@gmail.com>
2022-02-01 14:09:22 +0000

    updated port

--- app.js
+++ app.js
@@ -1,36 +1,30 @@
 const express = require("express");
 const path = require("path");
-const hbs = require("hbs");
+// const hbs = require("hbs");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const app = express();
 
 // Define paths for Express config
-const publicPath = path.join(__dirname, "./public")
-const viewsPath = path.join(__dirname, "./templates/views")
-const partialsPath = path.join(__dirname, "./templates/partials")
+// const publicPath = path.join(__dirname, "./public")
+// const viewsPath = path.join(__dirname, "./templates/views")
+// const partialsPath = path.join(__dirname, "./templates/partials")
 
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+// app.set('view engine', 'hbs')
+// app.set('views', viewsPath)
+// hbs.registerPartials(partialsPath)
 
 // set public directory
-app.use(express.static(publicPath))
+// app.use(express.static(publicPath))
 
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.send('Working ⚛️')
 })
-
-// 404
-app.get('*', (req, res) => {
-  res.render('404')
-})
-
 
 // start up server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Listening on port ${port}`);
 })
