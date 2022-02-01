8702546ebea9e5841c0372b770fbb8196005d8a1
Conor Hagan <conorhagan7@gmail.com>
2022-02-01 14:22:00 +0000

    update routes

--- app.js
+++ app.js
@@ -7,21 +7,20 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // Define paths for Express config
-// const publicPath = path.join(__dirname, "./public")
-// const viewsPath = path.join(__dirname, "./templates/views")
-// const partialsPath = path.join(__dirname, "./templates/partials")
+const publicPath = path.join(__dirname, "./public")
+const viewsPath = path.join(__dirname, "./templates/views")
+const partialsPath = path.join(__dirname, "./templates/partials")
 
 // Setup handlebars engine and views location
-// app.set('view engine', 'hbs')
-// app.set('views', viewsPath)
-// hbs.registerPartials(partialsPath)
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // set public directory
-// app.use(express.static(publicPath))
-
+app.use(express.static(publicPath))
 
 app.get('/', (req, res) => {
-  res.send('Working ⚛️')
+  res.render('index')
 })
 
 // start up server
